b6df7f60a8b7b1e49fa943bef0bec59a77744d86
Vansh Goswami <vanshgoswami40@gmail.com>
2025-09-28 22:25:36 +0530

    Learning React

---
+++ functions.js
@@ -0,0 +1,8 @@
+
+const container= document.querySelector(".container")
+const click = document.querySelector("#click")
+
+click.addEventListener("click", function(){
+    console.log("vansh");
+    
+})
